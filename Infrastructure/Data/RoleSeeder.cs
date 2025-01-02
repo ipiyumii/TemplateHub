fa805476d6792035f4f61860691d1d5078d2ff7c
Piyumi Warnakulasuriya <sendtopiyumi@gmail.com>
2025-01-03 01:43:21 +0530

    role based auth

--- Infrastructure/Data/RoleSeeder.cs
+++ Infrastructure/Data/RoleSeeder.cs
@@ -4,7 +4,20 @@ namespace boilerplate_app.Infrastructure.Data
 {
     public class RoleSeeder
     {
-       
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "User", "Manager", "Admin" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
     }
 }
  
