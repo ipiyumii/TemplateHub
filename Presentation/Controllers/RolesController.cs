07aaca34dee911d87063c96d238619d044e55e1e
Piyumi Warnakulasuriya <sendtopiyumi.com>
2025-01-03 01:42:45 +0530

    role based auth

--- Presentation/Controllers/RolesController.cs
+++ Presentation/Controllers/RolesController.cs
@@ -13,7 +13,39 @@ namespace boilerplate_app.Presentation.Controllers
 
     public class RolesController : ControllerBase
     {
-       
+        UserManager<User> _userManager;
+        RoleManager<IdentityRole> _roleManager;
+        public RolesController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        [HttpPost("assign-role")]
+        [Authorize(Roles = "Admin")]
+
+        public async Task<IActionResult> AssignRole(AssignRoleDto assignRoleDto)
+        {
+            var user = await _userManager.FindByNameAsync(assignRoleDto.UserName);
+            if (user == null)
+            {
+                return NotFound("User not found");
+            }
+
+            if (!await _roleManager.RoleExistsAsync(assignRoleDto.Role))
+            {
+                return BadRequest("Role does not exist");
+            }
+
+            var result = await _userManager.AddToRoleAsync(user, assignRoleDto.Role);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(result.Errors);
+            }
+
+            return Ok($"Role '{assignRoleDto.Role}' assigned to user '{assignRoleDto.UserName}' successfully.");
+        }
 
 
     }
