11bc48e4b700006f34b8116175dca08e98df4468
Piyumi Warnakulasuriya <sendtopiyumi@gmail.com>
2025-01-04 23:26:57 +0530

    update

--- Infrastructure/Data/RoleSeeder.cs
+++ Infrastructure/Data/RoleSeeder.cs
@@ -4,7 +4,39 @@ namespace boilerplate_app.Infrastructure.Data
 {
     public class RoleSeeder
     {
-        
+        public static async Task SeedRolesAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "User", "Manager", "Admin" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+
+        public static async Task SeedAdminAsync(IServiceProvider serviceProvider)
+        {
+            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+            string email = "admin@gmail.com";
+            string password = "password12@";
+
+            if (await userManager.FindByEmailAsync(email) == null)
+            {
+                var user = new IdentityUser();
+                user.UserName = email;
+                user.Email = email;
+
+                await userManager.CreateAsync(user, password);
+                await userManager.AddToRoleAsync(user, "Admin");
+            }
+
+        }
     }
 }
  
